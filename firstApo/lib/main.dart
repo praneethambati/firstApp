e3240696a28aa8cdf27efa2c88cc1bce6d661a6d
Josh Praneeth <joshpraneeth.ambati@gmail.com>
2020-11-18 11:29:18 +0530

    Changed text widget

--- firstApo/lib/main.dart
+++ firstApo/lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'I have pushed the button this many times:',
+              'Pushed the button this many times:',
             ),
             Text(
               '$_counter',
